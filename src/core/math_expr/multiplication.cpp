90b65644d58d5e9d83e6e949d5574c6d45ef7e80
Roman Remeslo <remesloroman@gmail.com>
2025-08-14 12:21:17 +0200

    Add Multiplication Operation - Inherits from Expression - Represents mathematical multiplication operation

---
+++ src/core/math_expr/multiplication.cpp
@@ -0,0 +1,17 @@
+#include "multiplication.hpp"
+
+Multiplication::Multiplication(ExprPtr lhs, ExprPtr rhs) : lhs_(std::move(lhs)), rhs_(std::move(rhs)){
+
+}
+
+FloatT Multiplication::eval() const{
+    return lhs_->eval() * rhs_->eval();
+}
+
+std::ostream &Multiplication::print(std::ostream &os) const{
+    return os << *lhs_ << " * " << *rhs_;
+}
+
+std::ostream &operator<<(std::ostream &os, const Multiplication &mult_expr){
+    return mult_expr.print(os);
+}
---
+++ src/core/math_expr/multiplication.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "expression.hpp"
+
+class Multiplication : public Expression
+{
+public:
+    Multiplication(ExprPtr lhs, ExprPtr rhs);
+
+    FloatT eval() const override;
+
+    std::ostream &print(std::ostream &os) const override;
+
+    friend std::ostream &operator<<(std::ostream &os, const Multiplication &mult_expr); 
+private: 
+    ExprPtr lhs_;
+    ExprPtr rhs_;
+};
