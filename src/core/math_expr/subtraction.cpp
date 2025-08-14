e34dd6003b2ef6e6af6d131f7f42a0b8fccd3ba8
Ivanov <ivanovtimofey1@gmail.com>
2025-08-14 12:16:30 +0200

    Add Subtraction Class - Inherits from Expression - Represents mathematical subtraction operation

---
+++ src/core/math_expr/subtraction.cpp
@@ -0,0 +1,17 @@
+#include "subtraction.hpp"
+
+Subtraction::Subtraction(ExprPtr lhs, ExprPtr rhs) : lhs_(std::move(lhs)), rhs_(std::move(rhs)){
+
+}
+
+FloatT Subtraction::eval() const{
+    return lhs_->eval() - rhs_->eval();
+}
+
+std::ostream &Subtraction::print(std::ostream &os) const{
+    return os << *lhs_ << " - " << *rhs_;
+}
+
+std::ostream &operator<<(std::ostream &os, const Subtraction &add_expr){
+    return add_expr.print(os);
+}
---
+++ src/core/math_expr/subtraction.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "expression.hpp"
+
+class Subtraction : public Expression
+{
+public:
+    Subtraction(ExprPtr lhs, ExprPtr rhs);
+
+    FloatT eval() const override;
+
+    std::ostream &print(std::ostream &os) const override;
+
+    friend std::ostream &operator<<(std::ostream &os, const Addition &add_expr); 
+private: 
+    ExprPtr lhs_;
+    ExprPtr rhs_;
+};
