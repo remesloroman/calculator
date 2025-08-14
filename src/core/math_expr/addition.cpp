4ae898068defa5b3fb2843bf950a67d3541658b7
Ivanov <ivanovtimofey1@gmail.com>
2025-08-14 12:04:15 +0200

    Add Addition Class - Inherits from Expression - Represents mathematical addition operation

---
+++ src/core/math_expr/addition.cpp
@@ -0,0 +1,17 @@
+#include "addition.hpp"
+
+Addition::Addition(ExprPtr lhs, ExprPtr rhs) : lhs_(std::move(lhs)), rhs_(std::move(rhs)){
+
+}
+
+FloatT Addition::eval() const{
+    return lhs_->eval() + rhs_->eval();
+}
+
+std::ostream &Addition::print(std::ostream &os) const{
+    return os << *lhs_ << " + " << *rhs_;
+}
+
+std::ostream &operator<<(std::ostream &os, const Addition &add_expr){
+    return add_expr.print(os);
+}
