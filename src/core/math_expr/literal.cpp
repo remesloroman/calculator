2dcc83ce1557cf685a2f99d17ed04f5bbb7550f7
Roman Remeslo <remesloroman@gmail.com>
2025-08-14 11:50:47 +0200

    Add Literal Expression Class

---
+++ src/core/math_expr/literal.cpp
@@ -0,0 +1,15 @@
+#include "literal.hpp"
+
+FloatT Literal::eval() const {
+    return value_;
+}
+
+std::ostream & Literal::print(std::ostream & os) const {
+    return os << value_;
+}
+
+std::ostream & operator<<(std::ostream & os, const Literal &expr) {
+    expr.print(os);
+    
+    return os;
+}
---
+++ src/core/math_expr/literal.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "expression.hpp"
+#include "../alias.hpp"
+#include <iostream>
+
+class Literal : public Expression {
+public:
+    FloatT eval() const override;
+
+    std::ostream & print(std::ostream & os) const override;
+
+    friend std::ostream & operator<<(std::ostream & os, const Literal &expr);
+private:
+    FloatT value_;
+};
