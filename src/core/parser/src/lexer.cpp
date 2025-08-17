b4d8c77d1b97dbef8259c9377c1870e3e39ef554
Roman Remeslo <remesloroman@gmail.com>
2025-08-17 13:05:54 +0200

    Refactor And Modify Lexer Class - Added public peek() and current() methods - Split next() method to constructor initialization and itterate() method - Add queue that contains tokens

--- src/core/parser/src/lexer.cpp
+++ src/core/parser/src/lexer.cpp
@@ -1,8 +1,18 @@
 #include "parser/lexer.hpp"
 #include "parser/token_type_utils.hpp"
 
-Lexer::Lexer(std::string is) : source_(is)
+Lexer::Lexer(std::string is) : source_(is), current_(TokenType::EndOfFile)
 {
+    Token current = itterate();
+    tokens_.push(current);
+
+    // Init current_
+    current_ = current;
+
+    while(current.type() != TokenType::EndOfFile) {
+        current = itterate();
+        tokens_.push(current);
+    }
 }
 
 void Lexer::skipSpaces()
@@ -25,24 +35,38 @@ bool Lexer::checkEOF()
     return false;
 }
 
-Token Lexer::next()
+Token Lexer::next() {
+    current_ = tokens_.front();
+    tokens_.pop();
+
+    return current_;
+}
+
+Token Lexer::peek() {
+    return tokens_.front();
+}
+
+Token Lexer::itterate()
 {
     skipSpaces();
 
-    if (checkEOF())
-        return Token(TokenType::EndOfFile);
+    if (checkEOF()) return Token(TokenType::EndOfFile);
 
     char symbol = source_.peek();
 
     if (std::isdigit(static_cast<unsigned char>(symbol)))
     {
         double value = 0.0;
-        if (!(source_ >> value))
-            return Token(TokenType::Error);
+        if (!(source_ >> value)) return Token(TokenType::Error);
 
         return Token(value);
     }
 
     source_.get();
+
     return Token(getCharTokenType(symbol));
+}
+
+Token Lexer::current() {
+    return current_;
 }
