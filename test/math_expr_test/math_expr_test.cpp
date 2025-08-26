c7e43e8a04e26fe6d33f186359628eb204553eba
Roman Remeslo <remesloroman@gmail.com>
2025-08-26 14:22:20 +0200

    Add Tests Accordingly New Structure Of math_expr

--- test/math_expr_test/math_expr_test.cpp
+++ test/math_expr_test/math_expr_test.cpp
@@ -4,6 +4,7 @@
 #include <math_expr/literal.hpp>
 #include <math_expr/addition.hpp>
 #include <math_expr/multiplication.hpp>
+#include <math_expr/unary_subtraction.hpp>
 
 int main()
 {
@@ -16,6 +17,12 @@ int main()
     
         Addition *all = new Addition(std::make_unique<Literal>(3.), std::make_unique<Literal>(5.));
         assert(all->eval() == 8.);
+    }
+
+    {
+        UnarySubtraction *un_expr = new UnarySubtraction(std::make_unique<Literal>(55));
+
+        assert(un_expr->eval() == -55);
     }
 
     return 0;
